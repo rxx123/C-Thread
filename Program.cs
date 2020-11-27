c8018a0612b88e2b31388c3d4aafc7f6e0e4bd72
ruxingxing <549589768@qq.com>
2020-11-27 10:20:51 +0800

    线程提交

--- Program.cs
+++ Program.cs
@@ -7,20 +7,20 @@ namespace ThreadDemo
     {
         static void Main(string[] args)
         {
-            //Thread.CurrentThread.Name = "Main Thread ...";
-            //Thread t = new Thread(WriteY);
-            //t.Name = "Y Thread ...";
-            //t.Start();
-            //Console.WriteLine(Thread.CurrentThread.Name);
-            //for (int i = 0; i < 1000; i++)
-            //{
-            //    Console.Write("x");
-            //}            
-
+            Thread.CurrentThread.Name = "Main Thread ...";
             Thread t = new Thread(WriteY);
+            t.Name = "Y Thread ...";
             t.Start();
+            Console.WriteLine(Thread.CurrentThread.Name);
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("x");
+            }
+
+            //Thread t = new Thread(WriteY);
+            //t.Start();
             //t.Join();
-            Console.WriteLine("Thread t has ended！");
+            //Console.WriteLine("Thread t has ended！");
         }
         static void WriteY()
         {
