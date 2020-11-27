161c9f8fd1d61b59eec0e7a5133a6359394f0d75
ruxingxing <549589768@qq.com>
2020-11-27 10:13:02 +0800

    初始化

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread.CurrentThread.Name = "Main Thread ...";
+            //Thread t = new Thread(WriteY);
+            //t.Name = "Y Thread ...";
+            //t.Start();
+            //Console.WriteLine(Thread.CurrentThread.Name);
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    Console.Write("x");
+            //}            
+
+            Thread t = new Thread(WriteY);
+            t.Start();
+            //t.Join();
+            Console.WriteLine("Thread t has ended！");
+        }
+        static void WriteY()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name);
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("y");
+            }
+        }
+    }
+}
